d59555820edf3d9e63ff13c4f955e427d22a9690
zlbandy <zlbandy@163.com>
2011-12-31 10:44:05 +0800

    modify login style

--- src/com/chinaece/gaia/gui/MainActivity.java
+++ src/com/chinaece/gaia/gui/MainActivity.java
@@ -3,6 +3,7 @@ package com.chinaece.gaia.gui;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -19,70 +20,73 @@ import android.widget.SimpleAdapter;
 import com.chinaece.gaia.R;
 import com.chinaece.gaia.db.DataStorage;
 import com.chinaece.gaia.http.OAHttpApi;
+import com.chinaece.gaia.types.AppType;
 
 public class MainActivity extends Activity {
 	String token;
 	private URL formatUrl;
+	private boolean flag;
 
 	/** Called when the activity is first created. */
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		DataStorage.load(MainActivity.this);
-		token = DataStorage.properties.get("token").toString();
-		try {
-			formatUrl = new URL(DataStorage.properties.get("url").toString());
-			ApiTask task = new ApiTask();
-			task.execute(formatUrl.toString(), token.toString());
-		} catch (MalformedURLException e) {
-		}
-		setContentView(R.layout.mainlayout);
-		GridView gridview = (GridView) findViewById(R.id.gridview);
-		Integer[] images = { R.drawable.userinfo,
-        		R.drawable.datalibrary,R.drawable.weatherforecast,
-        		R.drawable.ecenewspaper,R.drawable.announcement,
-        		R.drawable.pendings,R.drawable.urgentwarning,
-        		R.drawable.contact
-        		};
-		ArrayList<HashMap<String, Object>> meumList = new ArrayList<HashMap<String, Object>>();
-		String[] mainmenu = { "个人信息", "资料馆", "天气预报", "华东有色报", "公告", "待办提醒",
-				"紧急公文" , "通信录"};
-		for (int i = 0; i < mainmenu.length; i++) {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("ItemImage", images[i]);
-			map.put("ItemText", mainmenu[i]);
-			meumList.add(map);
-		}
-		SimpleAdapter saMenuItem = new SimpleAdapter(this, meumList,
-				R.layout.menuitem, new String[] { "ItemImage", "ItemText" },
-				new int[] { R.id.ItemImage, R.id.ItemText });
-		gridview.setAdapter(saMenuItem);
-		gridview.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				switch (arg2) {
-				case 2:
-					Intent intent2 = new Intent(MainActivity.this,
-							WeatherActivity.class);
-					startActivity(intent2);
-					break;
-				case 5:
-					Intent intent = new Intent(MainActivity.this,
-							PendingsActivity.class);
-					startActivity(intent);
-					break;
-				case 7:
-					Intent intent7 = new Intent(MainActivity.this,
-							ContactsActivity.class);
-					startActivity(intent7);
-					break;
-				default:
-					break;
-				}
+			DataStorage.load(MainActivity.this);
+			token = DataStorage.properties.get("token").toString();
+			try {
+				formatUrl = new URL(DataStorage.properties.get("url").toString());
+				ApiTask task = new ApiTask();
+				task.execute(formatUrl.toString(), token.toString());
+			} catch (MalformedURLException e) {
 			}
-		});
-	}
+			setContentView(R.layout.mainlayout);
+			GridView gridview = (GridView) findViewById(R.id.gridview);
+			Integer[] images = { R.drawable.userinfo,
+	        		R.drawable.datalibrary,R.drawable.weatherforecast,
+	        		R.drawable.ecenewspaper,R.drawable.announcement,
+	        		R.drawable.pendings,R.drawable.urgentwarning,
+	        		R.drawable.contact
+	        		};
+			ArrayList<HashMap<String, Object>> meumList = new ArrayList<HashMap<String, Object>>();
+			String[] mainmenu = { "个人信息", "资料馆", "天气预报", "华东有色报", "公告", "待办提醒",
+					"紧急公文" , "通信录"};
+			for (int i = 0; i < mainmenu.length; i++) {
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				map.put("ItemImage", images[i]);
+				map.put("ItemText", mainmenu[i]);
+				meumList.add(map);
+			}
+			SimpleAdapter saMenuItem = new SimpleAdapter(this, meumList,
+					R.layout.menuitem, new String[] { "ItemImage", "ItemText" },
+					new int[] { R.id.ItemImage, R.id.ItemText });
+			gridview.setAdapter(saMenuItem);
+			gridview.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+						long arg3) {
+					switch (arg2) {
+					case 2:
+						Intent intent2 = new Intent(MainActivity.this,
+								WeatherActivity.class);
+						startActivity(intent2);
+						break;
+					case 5:
+						Intent intent = new Intent(MainActivity.this,
+								PendingsActivity.class);
+						startActivity(intent);
+						break;
+					case 7:
+						Intent intent7 = new Intent(MainActivity.this,
+								ContactsActivity.class);
+						startActivity(intent7);
+						break;
+					default:
+						break;
+					}
+				}
+			});
+		}
+	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,6 +106,7 @@ public class MainActivity extends Activity {
 		}
 		return false;
 	}
+	
 
     class ApiTask extends AsyncTask<String, Integer, Boolean> {
 		private ProgressDialog dialog;
@@ -121,6 +126,7 @@ public class MainActivity extends Activity {
 
 		@Override
 		protected void onPostExecute(Boolean flag) {
+			MainActivity.this.flag = flag;
 			dialog.dismiss();
 		}
 
