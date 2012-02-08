fff05adf9cda42d765529006675a63f0e7dc71e8
techzhou <yzlkester@gmail.com>
2012-02-08 09:58:09 +0800

    增加通知工具类  close #8

---
+++ src/com/chinaece/gaia/util/NotificationCenter.java
@@ -0,0 +1,27 @@
+package com.chinaece.gaia.util;
+
+import com.chinaece.gaia.R;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationCenter {
+	
+	private static NotificationManager mNotificationManager ;
+	private static int NOTIFICATION_ID = 1;
+	
+	public static void sendNotification(Intent intent, Context context, String tip, String title, String content){
+		if(mNotificationManager == null)
+			mNotificationManager =  (NotificationManager)context.getSystemService(Activity.NOTIFICATION_SERVICE);
+		Notification mNotification = new Notification(R.drawable.ic_launcher, tip, System.currentTimeMillis());
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);      
+        PendingIntent mContentIntent =PendingIntent.getActivity(context,0, intent, 0);  
+        mNotification.setLatestEventInfo(context, title, content, mContentIntent);  
+        mNotificationManager.notify(NOTIFICATION_ID++, mNotification); 
+	}
+
+}
