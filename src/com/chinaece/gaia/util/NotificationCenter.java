708758163909a9c30c7c3a0306f99b4a70f3d0ae
techzhou <yzlkester@gmail.com>
2012-03-08 10:06:40 +0800

    优化通知中心 && 添加定时清除的通知

--- src/com/chinaece/gaia/util/NotificationCenter.java
+++ src/com/chinaece/gaia/util/NotificationCenter.java
@@ -1,6 +1,7 @@
 package com.chinaece.gaia.util;
 
-import com.chinaece.gaia.R;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.app.Notification;
@@ -9,6 +10,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
+import com.chinaece.gaia.R;
+
 public class NotificationCenter {
 	
 	private static NotificationManager mNotificationManager ;
@@ -16,31 +19,44 @@ public class NotificationCenter {
 	public static int PENDING_NOTIFICATION_ID = 65536;
 	public static int ONGOING_NOTIFICATION_ID = 65535;
 	
-	private static void sendNotification(Intent intent, Context context, String tip, String title, String content, int ID){
+	private static void sendNotification(Intent intent, Context context, String tip, String title, String content, int ID, int flags){
 		if(mNotificationManager == null)
 			mNotificationManager =  (NotificationManager)context.getSystemService(Activity.NOTIFICATION_SERVICE);
 		Notification mNotification = new Notification(R.drawable.appicon, tip, System.currentTimeMillis());
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  
-		mNotification.flags |= Notification.FLAG_AUTO_CANCEL;
-        PendingIntent mContentIntent =PendingIntent.getActivity(context,0, intent, 0);  
-        mNotification.setLatestEventInfo(context, title, content, mContentIntent);  
+		mNotification.flags = flags;
+		if(intent != null){
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		}
+		PendingIntent mContentIntent = PendingIntent.getActivity(context,0, intent, 0);;
+		mNotification.setLatestEventInfo(context, title, content, mContentIntent);  
         mNotificationManager.notify(ID, mNotification); 
 	}                                                               
 	
 	public static void sendNormalNotification(Intent intent, Context context, String tip, String title, String content){
-		sendNotification(intent, context, tip, title, content, NOTIFICATION_ID++);
+		sendNotification(intent, context, tip, title, content, NOTIFICATION_ID++, Notification.FLAG_AUTO_CANCEL);
 	}
 	
 	public static void sendPendingsNotification(Intent intent, Context context, String tip, String title, String content){
-		sendNotification(intent, context, tip, title, content, PENDING_NOTIFICATION_ID);
+		sendNotification(intent, context, tip, title, content, PENDING_NOTIFICATION_ID, Notification.FLAG_AUTO_CANCEL);
 	}
 	
 	public static void sendOngoingNotification(Intent intent, Context context, String tip, String title, String content)
 	{
-		sendNotification(intent, context, tip, title, content,ONGOING_NOTIFICATION_ID);
-		Notification mNotification = new Notification(R.drawable.appicon, tip, System.currentTimeMillis());
-		mNotification.flags |= Notification.FLAG_ONGOING_EVENT;
-		mNotification.flags |= Notification.FLAG_NO_CLEAR;
+		sendNotification(intent, context, tip, title, content,ONGOING_NOTIFICATION_ID, Notification.FLAG_ONGOING_EVENT|Notification.FLAG_NO_CLEAR);
+	}
+	
+	public static void sendTimerNotification(Intent intent, Context context, String tip, String title, String content, int time){
+		final int nid = NOTIFICATION_ID;
+		sendNormalNotification(intent, context, tip, title, content);
+		TimerTask cancelTask = new TimerTask() {
+			
+			@Override
+			public void run() {
+				NotificationCenter.clearNotification(nid);
+			}
+		};
+		Timer timer = new Timer(true);
+		timer.schedule(cancelTask, time * 1000);
 	}
 	
 	public static void clearNotification(int id){
